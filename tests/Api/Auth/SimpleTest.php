de6745f0ddba3c5ad78b5bf94b8e8cc167272f90
Gercil <gerciljunio@gmail.com>
2025-10-18 19:27:36 -0300

    alterando as etapas para escrita em portugues
    Signed-off-by: Gercil <gerciljunio@gmail.com>

--- tests/Api/Auth/SimpleTest.php
+++ tests/Api/Auth/SimpleTest.php
@@ -3,21 +3,21 @@
 use Tests\Support\Env;
 
 describe('api login', function () {
-    it('env has testing string on APP_ENV key', function () {
+    it('existe a variável APP_ENV no ambiente de testes e o valor é testing', function () {
         expect(Env::get('APP_ENV'))->toBe('testing');
     });
 
-    it('get response status 200', function (array $headers) {
+    it('a resposta tem o status 200 e envia os headers', function (array $headers) {
         $resp = http()->withHeaders($headers)->get('/objects');
         expect($resp->status())->toBe(200);
     })->with('headers');
 
-    it('response body is a valid json', function () {
+    it('a resposta tem um JSON válido', function () {
         $resp = http()->get('/objects');
         expect(json_validate($resp->body()))->toBeTrue();
     });
 
-    it('response body is array for objects', function () {
+    it('a resposta é um array de objetos', function () {
         $resp = http()->get('/objects');
         expect($resp->json())->toBeArray();
     });
